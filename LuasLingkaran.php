c890681841765dabe297e085786d64cb33c2fc4c
Yusuf Eka Wicaksana <ekayusuf.wicaksana@gmail.com>
2025-10-10 14:48:53 +0700

    tambah fitur luas lingkaran

---
+++ LuasLingkaran.php
@@ -0,0 +1,14 @@
+<?php
+
+class LuasLingkaran {
+    
+    public const phi = 3.14;
+    public int $jari;
+}
+
+$lingkaran = new LuasLingkaran();
+$lingkaran->jari = 7;
+
+$rumus = LuasLingkaran::phi * $lingkaran->jari * $lingkaran->jari;
+
+echo "Hasilnya adalah: ". $rumus;
